b448d3e7312ef36ba57928c1bdb81645d7826ff8
Ryan Warner <ryan.warner@gatech.edu>
2023-04-03 14:31:33 -0400

    Correct merge error

--- components/header.js
+++ components/header.js
@@ -1,5 +1,4 @@
 export default function Header(props) {
-export default function Header(props) {
     return (
         <div className="flex sm:flex-row gap-2 justify-start flex-col w-full sm:justify-between items-center p-4 mb-4 shadow-lg bg-[#1890ff]">
             <div className="flex flex-col self-start sm:self-center text-white">
