020d6076c400aa12f085c75f28e33580f7efcdb1
Ryan Warner <ryan.warner@gatech.edu>
2023-04-03 14:30:01 -0400

    Last tweaks

--- components/header.js
+++ components/header.js
@@ -1,14 +1,16 @@
-import { Button } from 'antd'
-
-export default function Header() {
+export default function Header(props) {
     return (
-        <div className="flex w-full justify-between items-center">
-            <div className="text-4xl font-bold pb-4">
-                First Credit Union
+        <div className="flex sm:flex-row gap-2 justify-start flex-col w-full sm:justify-between items-center p-4 mb-4 shadow-lg bg-[#1890ff]">
+            <div className="flex flex-col self-start sm:self-center text-white">
+                <div className="text-4xl font-bold">
+                    First Credit Union
+                </div>
+                <div className="font-semibold">Powered by Vertice AI</div>
             </div>
-            <Button type="primary" className="rounded-xl" >
-                BUTTON
-            </Button>
+            <div className="flex flex-col justify-end self-start sm:self-center text-white">
+                <div className="self-end text-lg font-semibold">{`Total Members: ${props.totalMembers.toLocaleString("en-US")}`}</div>
+                <div className="self-end text-lg font-semibold">{`Total Products: ${props.totalProducts.toLocaleString("en-US")}`}</div>
+            </div>
         </div>
     )
 }
